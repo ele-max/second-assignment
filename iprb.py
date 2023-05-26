2091c2a9b2783e42f619c623a3bdeb495a844c39
elenaorsini <59398721+ele-max@users.noreply.github.com>
2023-05-26 12:13:14 +0200

    Add files via upload

---
+++ iprb.py
@@ -0,0 +1,9 @@
+k = 19                                                         
+m = 29                                                        
+n = 16     
+
+summ = k + m + n                                            
+
+prob = (4*(k*(k-1)+2*k*m+2*k*n+m*n)+3*m*(m-1))/(4*summ*(summ-1))
+
+print(prob)  
